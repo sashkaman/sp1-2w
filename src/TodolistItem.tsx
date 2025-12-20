a1417f822f226d21b05532f61b6d5a978c4c9a89
Alexander <rzn.russia4912@gmail.com>
2025-12-20 15:48:39 +0300

    add onClick button

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -23,6 +23,7 @@ export const TodolistItem = ({ title, tasks }: Props) => {
               <li key={task.id}>
                 <input type="checkbox" checked={task.isDone} />
                 <span>{task.title}</span>
+                <button onClick={() => alert(task.id)}>x</button>
               </li>
             )
           })}
